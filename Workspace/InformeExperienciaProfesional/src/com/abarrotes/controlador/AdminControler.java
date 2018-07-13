6e6a5c6b794bc7cfe600b61846c60477fc693508
Alfonso Angulo <alfonsoapple@me.com>
2018-07-12 22:28:27 -0500

    Nuevo Cambio Vamos bien
    Ya esta el Servlet casi listo

--- Workspace/InformeExperienciaProfesional/src/com/abarrotes/controlador/AdminControler.java
+++ Workspace/InformeExperienciaProfesional/src/com/abarrotes/controlador/AdminControler.java
@@ -1,6 +1,8 @@
 package com.abarrotes.controlador;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,17 +21,13 @@ public class AdminControler extends HttpServlet {
      */
     public AdminControler() {
         super();
-        // TODO Auto-generated constructor stub
+        System.out.println("SERVLET");
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String usuario = request.getParameter("accesID");
-		String password = request.getParameter("passID");
-		System.out.println("EXITO: " + usuario + " > " + password);
-		
 		
 	}
 
@@ -37,8 +35,16 @@ public class AdminControler extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String usuario = request.getParameter("accesID");
+		String password = request.getParameter("passID");
+		System.out.println("EXITO: " + usuario + " > " + password);
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("Name saved in Request Object: " + usuario + "<br>");
+		out.println("Name saved in Request Object: " + password + "<br>");
+		out.println("</html>");
 	}
 
 }
