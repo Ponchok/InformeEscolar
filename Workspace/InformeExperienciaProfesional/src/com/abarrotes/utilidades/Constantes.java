d06b6136c88eebd6f487698bad6d4f7b5aa255bc
Alfonso Angulo <alfonsoapple@me.com>
2018-07-21 02:31:49 -0500

    3

--- Workspace/InformeExperienciaProfesional/src/com/abarrotes/utilidades/Constantes.java
+++ Workspace/InformeExperienciaProfesional/src/com/abarrotes/utilidades/Constantes.java
@@ -8,8 +8,8 @@ public class Constantes {
 	public static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
 	public static final String DAO_TABLA_ACCESO = "select * from acceso;";
 	public static final String DAO_TABLA_EMPLEADOS = "select  * from empleado;";
-	public static final String DAO_TABLA_ARTICULOS = "select * from SandBoxAbarrote.new_view;"; //De esta vista tratar de cambiar el nombre y agregarla a la DB de PROD
-	public static final String DAO_TABLA_SELECCION_ARTICULOS_1 = "select nombre as Nombre, descripcion as Descripcion, costo as Precio from articulo where idProducto IN (";
+	public static final String DAO_TABLA_ARTICULOS = "select * from SandBoxAbarrote.tabla_articulos;"; //De esta vista tratar de cambiar el nombre y agregarla a la DB de PROD
+	public static final String DAO_TABLA_SELECCION_ARTICULOS_1 = "select nombre as Nombre, descripcion as Descripcion, precio as Precio from articulo where idProducto IN (";
 	public static final String DAO_TABLA_SELECCION_ARTICULOS_2 = ");";
 	public static final String DAO_OBTENER_ID_VENTA = "select count(idVenta)  from ventas;";
 	public static final String DAO_INSERTAR_VENTA = "INSERT INTO SandBoxAbarrote.ventas (idVenta, fecha) VALUES (?,?);";
